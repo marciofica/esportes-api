dfe7185e4c4f223812673db477be6b261b0ea7e6
Márcio Figueiredo Cardoso <marciofica@gmail.com>
2024-03-25 11:24:23 -0300

    ESP-2

--- src/main/java/com/serviconanuvem/esportes/EsportesApplication.java
+++ src/main/java/com/serviconanuvem/esportes/EsportesApplication.java
@@ -8,16 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class EsportesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EsportesApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public ResponseEntity<String> sayHello() {
-		return new ResponseEntity<>("Hello :)", HttpStatus.OK);
 	}
 
 }
